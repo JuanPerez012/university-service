7934e695f5997c07b8b2df567a737a9bd62e78d4
JuanPerez012 <juanfer.perez0421@gmail.com>
2025-10-21 12:51:14 -0500

    refactor(university): add configuration file

--- target/generated-sources/annotations/com/universitymicroservice/mapper/UniversityMapperImpl.java
+++ target/generated-sources/annotations/com/universitymicroservice/mapper/UniversityMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-20T23:57:18-0500",
+    date = "2025-10-21T12:42:42-0500",
     comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
